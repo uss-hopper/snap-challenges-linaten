cae7cae5692f73741fc9392005ee34d6a13eec1e
Lindsey Atencio <lindsey.atencio@gmail.com>
2019-10-09 14:58:53 -0600

    trying to recommit with all files

---
+++ snapchallengeslinaten/functions.js
@@ -0,0 +1,13 @@
+function add(var1, var2) {
+	return var1+var2;
+};
+$myVariable = add(1,2);
+console.log($myVariable);
+
+function concat(var1, var2){
+	return var1+var2;
+};
+$myString = concat("cats", "dogs");
+console.log($myString);
+
+
---
+++ snapchallengeslinaten/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE=HTML>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Snap Challenge</title>
+	<script src="functions.js">
+	</script>
+</head>
+<body>
+<h1>example of functions in js</h1>
+</body>
+</html>
