99db8bdc6ba7cd2cb22a0fd0997eed1fc423a127
Lindsey Atencio <lindsey.atencio@gmail.com>
2019-10-11 10:09:21 -0600

    snap two

---
+++ snap-challenge-10-11-2019/erd.php
@@ -0,0 +1,12 @@
+<!DOCTYPE=HTML>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>ERD</title>
+</head>
+<body>
+<div class="containter">
+	<img id="erd" src="erd.php">
+</div>
+</body>
+</html>
---
+++ snap-challenge-10-11-2019/story.php
@@ -0,0 +1,10 @@
+<!DOCTYPE=HTML>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>User Story</title>
+</head>
+<body>
+<p>As a casual user, I can upload an image.</p>
+</body>
+</html>
