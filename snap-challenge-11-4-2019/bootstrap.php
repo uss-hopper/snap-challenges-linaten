0b6ea302604a2df153bac23eb7e346e06d145de1
Lindsey Atencio <lindsey.atencio@gmail.com>
2019-11-04 09:24:47 -0700

    snap nine

---
+++ snap-challenge-11-4-2019/bootstrap.php
@@ -0,0 +1,13 @@
+<!DOCTYPE=html>
+<html lang="en">
+<head>
+	<link rel="stylesheet" href="style.css"/>
+	<meta charset="UTF-8">
+	<Title>Bootstrap</Title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+</head>
+<body>
+<button class="navbar-toggler" type="button"
+</body>
