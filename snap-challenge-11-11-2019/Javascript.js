cb83adc6d3f611e462db91a630038b7dee6501dc
Lindsey Atencio <lindsey.atencio@gmail.com>
2019-11-11 10:59:15 -0700

    snap ten

---
+++ snap-challenge-11-11-2019/Javascript.js
@@ -0,0 +1,10 @@
+var array1 = ['company'];
+
+array1.forEach(function(company) {
+	console.log(company);
+});
+
+
+// expected output: "a"
+// expected output: "b"
+// expected output: "c"
--- snap-challenge-11-4-2019/bootstrap.php
+++ snap-challenge-11-4-2019/bootstrap.php
@@ -9,5 +9,13 @@
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 </head>
 <body>
+<div class="container-fluid">
+	<div class="card-deck">
+		<div class="card border-light mb-5"></div>
+		<div class="card border-primary mn-5"></div>
+
+	</div>
+	<div class="container-fluid"></div>
+</div>
 <button class="navbar-toggler" type="button"
 </body>
