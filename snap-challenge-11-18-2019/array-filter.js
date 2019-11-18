ed3729b316fb4a8885ed13c2a6d984ec11a28cd0
Lindsey Atencio <lindsey.atencio@gmail.com>
2019-11-18 10:25:47 -0700

    snap 12

--- snap-challenge-11-18-2019/array-filter.js
+++ snap-challenge-11-18-2019/array-filter.js
@@ -1,14 +1,8 @@
-let cats=[{
-	Ensign Babyface! Your shields were failing, sir. This should be interesting. Your head is not an artifact! What's a knock-out like you doing in a computer-generated gin joint like this? Why don't we just give everybody a promotion and call it a night - 'Commander'? But the probability of making a six is no greater than that of rolling a seven. What? We're not at all alike! I recommend you don't fire until you're within 40,000 kilometers. Wouldn't that bring about chaos? Flair is what marks the difference between artistry and mere competence.
-}]
+const cats= "Ensign Babyface! Your shields were failing, sir. This should be interesting. Your head is not an artifact! What's a knock-out like you doing in a computer-generated gin joint like this? Why don't we just give everybody a promotion and call it a night - 'Commander'? But the probability of making a six is no greater than that of rolling a seven. What? We're not at all alike! I recommend you don't fire until you're within 40,000 kilometers. Wouldn't that bring about chaos? Flair is what marks the difference between artistry and mere competence.";
 
-function filterCat(arrayOfCats, index, letter) {
-	var filteredCats = arrayOfCats.filter(function(word) {
-		return word[index] === letter;
-	});
-	return filteredCats
+/** break string into words **/
+/** != this means not equal in js **/
 
-	console.log(filterWords(arrayOfWords, 3, 'e'))
-
-
+const newCats = cats.split('').filter(word =>word.toLowerCase()[0] !='e').join('');
+console.log(newCats);
 
