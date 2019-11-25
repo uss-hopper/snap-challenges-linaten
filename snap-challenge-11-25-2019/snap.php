df752f1780f340d5ed2b719b3b73c4fe1feb3bae
Lindsey Atencio <lindsey.atencio@gmail.com>
2019-11-25 08:37:50 -0700

    snap 14

---
+++ snap-challenge-11-25-2019/snap.php
@@ -0,0 +1,35 @@
+<!DOCTYPE=html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<Titl>Pseudocode</Titl>
+<!-- 1. In pseudocode, write a function that takes a string and returns it with all vowels (not including y) removed.
+2. In pseudocode, write a function that takes an array of numbers and returns the greatest number in the array.
+3. In pseudocode, write a function that computes the first 100 numbers in the Fibonacci sequence. The Fibonacci sequence is created by adding the two previous numbers in the sequence together, and starts 1, 1, 2, 3, 5, 8... -->
+
+</head>
+<body>
+<p1>
+	object = array(cat, dog, bob, fix, fat, sam)
+	function removeVowels(str) {
+	return str.replace(/aeiou]/gi, ' ');
+	}
+</p1>
+<p2>
+object = array(1, 7, 9, 11, 2, 5)
+	function largestNumber(array){
+	return largest number
+	}
+</p2>
+<p3>
+	function fibonacci(num) {
+	if (num <= 1) return 1;
+	return fibonacci(num - 1) + fibonacci(num - 2);
+	}
+	object= array(fibonacci)
+	function = add position 0 and 1, repeat
+	console log results
+</p3>
+</body>
+</html>
+
