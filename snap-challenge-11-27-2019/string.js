7c24896592b77f16e58309c49909a2119725bb5d
Lindsey Atencio <lindsey.atencio@gmail.com>
2019-11-27 09:34:59 -0700

    snap 15

---
+++ snap-challenge-11-27-2019/string.js
@@ -0,0 +1,11 @@
+const elements = ["Deflector", "power", "at", "maximum.", "Energy", "discharge", "in", "six", "seconds.", "Warp", "reactor", "core", "primary", "coolant", "failure.", "Fluctuate", "phaser", "resonance", "frequencies.", "Resistance", "is", "futile.", "Recommend", "we", "adjust", "shield", "harmonics", "to", "the", "upper", "EM", "band", "when", "proceeding.", "These", "appear", "to", "be", "some", "kind", "of", "power-wave-guide", "conduits", "which", "allow", "them", "to", "work", "collectively", "as", "they", "perform", "ship", "functions.", "Increase", "deflector", "modulation", "to", "upper", "frequency", "band."]
+
+console.log(elements.join(''));
+
+const newArray = [Deflectorpoweratmaximum.Energydischargeinsixseconds.Warpreactorcoreprimarycoolantfailure.Fluctuatephaserresonancefrequencies.Resistanceisfutile.RecommendweadjustshieldharmonicstotheupperEMbandwhenproceeding.Theseappeartobesomekindofpower-wave-guideconduitswhichallowthemtoworkcollectivelyastheyperformshipfunctions.Increasedeflectormodulationtoupperfrequencyband.]
+
+console.log(newArray.split('.').join(''));
+
+
+alternatively : use join
+
